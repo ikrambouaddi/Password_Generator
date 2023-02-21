2c101eafb19b8dce7a6b09b188257b2a06def944
unknown <ikrambouaddi11@gmail.com>
2023-02-21 23:47:46 +0100

    updat

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ console.log('hello word')
 const spanLength = document.querySelector('.pass-length input'),
 options =document.querySelectorAll('.option input'),
 passourdInput=document.querySelector('.input-box input'),
+passEndicator=document.querySelector('.pass-indicator'),
