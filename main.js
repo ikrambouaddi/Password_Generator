30e81cf15d94658bc116c5b771481ab2de917cff
unknown <ikrambouaddi11@gmail.com>
2023-02-23 01:05:02 +0100

    updat

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ options =document.querySelectorAll('.option input'),
 passourdInput=document.querySelector('.input-box input'),
 passEndicator=document.querySelector('.pass-indicator'),
 copyIcon=document.querySelector('.input-box span'),
+
+
+generatorBtn = document.querySelector('.generatare-btn');
