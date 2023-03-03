70317b5eeec07fe6b791170b51b8ff086c8c4e4c
unknown <ikrambouaddi11@gmail.com>
2023-03-04 00:08:46 +0100

    new updat

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ passourdInput=document.querySelector('.input-box input'),
 passEndicator=document.querySelector('.pass-indicator'),
 copyIcon=document.querySelector('.input-box span'),
 
-
 generatorBtn = document.querySelector('.generatare-btn');
+
 console.log(options)
 
 const charcters = {
@@ -24,11 +24,12 @@ const generatrPassord =()=>{
     falseValue=false,
     passLenght = spanLength.value;
 
-
-    
     options.forEach(option => {//looping touch each option's check
+        console.log('11',option.checked)
         if(option.checked){
+            console.log("testtttttttttttttttttttt",option.id)  
             if(option.id !== 'exc-duplicate' && option.id !== 'spaces'){
+                console.log("ikram3ndhasah")
                 staticpassord += charcters[option.id]
             }else if(option.id === 'spaces'){
                 staticpassord += ` ${staticpassord}  ` 
@@ -39,11 +40,31 @@ const generatrPassord =()=>{
         }
     });
 
+    // console.log(staticpassord)
+for(let i=0; i < passLenght;i++){
+        console.log(i)
+        console.log("udehuiew", passLenght);
+        let randChat = staticpassord[Math.floor(Math.random() * staticpassord.length)];
+        
+    if(falseValue){
+        console.log("test", randChat, randomLength.includes(randChat))
+    !randomLength.includes(randChat) || randChat == " " ? randomLength += randChat : i--;
+
+    }else{
+        console.log('ikram')
+        randomLength +=randChat;
+    }
+    // randomLength = "testr"
+
+}
+// console.log(randomLength) 
+passourdInput.value = randomLength;
 }
 
 const passIndicatorFct =() =>{
     passEndicator.id =spanLength.value <= 8 ? "weak" : spanLength.value <= 16 ?"medium":"strong";
 }
+
 const updateSlider = () =>{
     // pass 
     document.querySelector('.pass-length span').innerText=spanLength.value;
@@ -59,12 +80,6 @@ const copyPass =()=>{
         copyIcon.innerText= 'copy_all'
     }, 1500);
 }
-
 copyIcon.addEventListener('click',copyPass)
 spanLength.addEventListener('input', updateSlider)
 generatorBtn.addEventListener('click',generatrPassord)
-
-
-
-
-// i finish this project 
